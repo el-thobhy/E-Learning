6d4473b073b98e718e177dcd89b8aebeefcf7823
auriwan <auriwan.yasper@sci.ui.ac.id>
2022-03-10 14:23:08 +0700

    Splash Activity and project preparetion

---
+++ app/src/main/java/com/elthobhy/elearning/presentation/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.elthobhy.elearning.presentation.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.elthobhy.elearning.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/elthobhy/elearning/presentation/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.elthobhy.elearning.presentation.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.elthobhy.elearning.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
