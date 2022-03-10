6f07a5c5c72ee643b8a4f3a56cefaaf073345a05
auriwan <auriwan.yasper@sci.ui.ac.id>
2022-03-10 14:54:39 +0700

    UI-login Activity

---
+++ app/src/main/java/com/elthobhy/elearning/presentation/login/LoginActivity.kt
@@ -0,0 +1,17 @@
+package com.elthobhy.elearning.presentation.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.elthobhy.elearning.R
+import com.elthobhy.elearning.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
