5584f61744e5a5ad42ca43761eda4c391ab14c43
auriwan <auriwan.yasper@sci.ui.ac.id>
2022-03-12 06:49:38 +0700

    Models-Menambahkan data Dummy dan Membuat Models

---
+++ app/src/main/java/com/elthobhy/elearning/models/Content.kt
@@ -0,0 +1,14 @@
+package com.elthobhy.elearning.models
+
+
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+import android.os.Parcelable
+
+@Parcelize
+data class Content(
+    @SerializedName("id_content")
+    val idContent: Int? = null,
+    @SerializedName("pages")
+    val pages: List<Page>? = null
+) : Parcelable
---
+++ app/src/main/java/com/elthobhy/elearning/models/PartsPage.kt
@@ -0,0 +1,14 @@
+package com.elthobhy.elearning.models
+
+
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+import android.os.Parcelable
+
+@Parcelize
+data class PartsPage(
+    @SerializedName("content")
+    val content: String? = null,
+    @SerializedName("type")
+    val type: String? = null
+) : Parcelable
