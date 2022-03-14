7050b76c06e3c62766ee29b5ca92f8079b616539
auriwan <auriwan.yasper@sci.ui.ac.id>
2022-03-14 08:29:55 +0700

    Firebase-Menampilkan data User di halaman MainActivity dan UserActivity

--- app/src/main/java/com/elthobhy/elearning/presentation/main/MainActivity.kt
+++ app/src/main/java/com/elthobhy/elearning/presentation/main/MainActivity.kt
@@ -8,27 +8,69 @@ import android.os.Looper
 import android.util.Log
 import android.view.inputmethod.EditorInfo
 import androidx.core.widget.addTextChangedListener
+import com.bumptech.glide.Glide
 import com.elthobhy.elearning.adapter.MaterialsAdapter
 import com.elthobhy.elearning.databinding.ActivityMainBinding
+import com.elthobhy.elearning.models.User
 import com.elthobhy.elearning.presentation.content.ContentActivity
 import com.elthobhy.elearning.presentation.user.UserActivity
 import com.elthobhy.elearning.repository.Repository
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
+import showDialogError
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var materialsAdapter: MaterialsAdapter
+    private lateinit var userDatabase : DatabaseReference
+    private var user : FirebaseUser? = null
+
+    private var listener = object : ValueEventListener{
+        override fun onDataChange(snapshot: DataSnapshot) {
+            hideLoading()
+            val user = snapshot.getValue(User::class.java)
+            user?.let {
+                binding.apply {
+                    tvNameUser.text = it.nameUser
+                    Glide.with(this@MainActivity)
+                        .load(it.avatarUser)
+                        .placeholder(android.R.color.darker_gray)
+                        .into(ivUser)
+                }
+            }
+        }
+
+        override fun onCancelled(error: DatabaseError) {
+            hideLoading()
+            Log.e("error", "onCancelled: ${error.message}", )
+            showDialogError(this@MainActivity,error.message)
+        }
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //init
+        user = FirebaseAuth.getInstance().currentUser
+        userDatabase = FirebaseDatabase.getInstance("https://elearning-project-f0895-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("users")
         materialsAdapter = MaterialsAdapter()
 
         getDataMaterial()
-
+        getDataFirebase()
         onAction()
+    }
+
+    private fun getDataFirebase() {
+        showLoading()
+        userDatabase
+            .child(user?.uid.toString())
+            .addValueEventListener(listener)
+
     }
 
     private fun getDataMaterial() {
@@ -76,6 +118,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             swipeMain.setOnRefreshListener {
+                getDataFirebase()
                 getDataMaterial()
             }
         }
--- app/src/main/java/com/elthobhy/elearning/presentation/user/UserActivity.kt
+++ app/src/main/java/com/elthobhy/elearning/presentation/user/UserActivity.kt
@@ -4,16 +4,54 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings.ACTION_LOCALE_SETTINGS
+import android.util.Log
+import com.bumptech.glide.Glide
 import com.elthobhy.elearning.R
 import com.elthobhy.elearning.databinding.ActivityUserBinding
+import com.elthobhy.elearning.models.User
 import com.elthobhy.elearning.presentation.changepassword.ChangePasswordActivity
 import com.elthobhy.elearning.presentation.login.LoginActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
+import showDialogError
 
 class UserActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityUserBinding
     private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var userDatabase : DatabaseReference
+    private var user : FirebaseUser? = null
+
+    private var listener = object : ValueEventListener{
+        override fun onDataChange(snapshot: DataSnapshot) {
+            hideLoading()
+            val user = snapshot.getValue(User::class.java)
+            user?.let {
+                binding.apply {
+                    tvNameUser.text = it.nameUser
+                    Glide.with(this@UserActivity)
+                        .load(it.avatarUser)
+                        .placeholder(android.R.color.darker_gray)
+                        .into(ivUser)
+                    tvEmailUser.text = it.emailUser
+                }
+            }
+        }
+
+        override fun onCancelled(error: DatabaseError) {
+            hideLoading()
+            Log.e("error", "onCancelled: ${error.message}", )
+            showDialogError(this@UserActivity, error.message)
+        }
+    }
+
+    private fun hideLoading() {
+        binding.swipeUser.isRefreshing = false
+    }
+    private fun showLoading() {
+        binding.swipeUser.isRefreshing = true
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,8 +59,19 @@ class UserActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         //Init
+        user = FirebaseAuth.getInstance().currentUser
+        userDatabase = FirebaseDatabase.getInstance("https://elearning-project-f0895-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("users")
         firebaseAuth = FirebaseAuth.getInstance()
+        getDataFirebase()
         onAction()
+
+    }
+
+    private fun getDataFirebase() {
+        showLoading()
+        userDatabase
+            .child(user?.uid.toString())
+            .addValueEventListener(listener)
     }
 
     private fun onAction() {
@@ -40,6 +89,9 @@ class UserActivity : AppCompatActivity() {
                 firebaseAuth.signOut()
                 startActivity(Intent(this@UserActivity, LoginActivity::class.java))
                 finishAffinity()
+            }
+            swipeUser.setOnRefreshListener {
+                getDataFirebase()
             }
         }
     }
